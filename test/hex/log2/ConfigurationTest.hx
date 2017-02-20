d1afcf38159f973fd33aea61e618ca824540f988
St3veV <stepan.vyterna@gmail.com>
2017-02-20 14:11:13 +0100

    gitignore + fix js target

--- test/hex/log2/ConfigurationTest.hx
+++ test/hex/log2/ConfigurationTest.hx
@@ -2,9 +2,7 @@ package hex.log2;
 import hex.log2.LoggerConfig;
 import hex.log2.configuration.BasicConfiguration;
 import hex.log2.helper.TestLogTarget;
-import hex.log2.layout.DefaultJavascriptConsoleLogLayout;
 import hex.log2.layout.DefaultTraceLogLayout;
-import hex.log2.target.JavaScriptConsoleLogTarget;
 import hex.log2.target.TraceLogTarget;
 import hex.unittest.assertion.Assert;
 
@@ -34,18 +32,12 @@ class ConfigurationTest
 		
 		//create log targets
 		var traceTarget = new TraceLogTarget("Trace", null, new DefaultTraceLogLayout());
-		#if js
-		var consoleTarget = new JavaScriptConsoleLogTarget("Console", null, new DefaultJavascriptConsoleLogLayout());
-		#end
 		var testTarget = new TestLogTarget("Test", null, null);
 		
 		//create a logger config and add targets
 		//(at this point we can also add filters to the configuration etc.)
 		var lc1:LoggerConfig = LoggerConfig.createLogger("hex", LogLevel.WARN, null, null); // Logger will only forward warnings+
 		lc1.addLogTarget(traceTarget, LogLevel.ALL, null); // Target will accept every event that arrives (in this case only warnings+ will be forwarded from the logger anyway)
-		#if js
-		lc1.addLogTarget(consoleTarget, LogLevel.ALL, null);
-		#end
 		lc1.addLogTarget(testTarget, LogLevel.ALL, null);
 		configuration.addLogger(lc1.name, lc1); //Add logger config to the configuration
 		
@@ -63,7 +55,6 @@ class ConfigurationTest
 		logger.warn(message, ["warn"]);
 		logger.error(message, ["error"]);
 		logger.fatal(message, ["fatal"]);
-		
 		
 		//-----
 		//Just an assert to make sure everything is loggin as it's supposed to
